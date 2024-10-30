a1301fb05c8f9635c8ec287b37406ad0c2ad5687
JayVaja345 <vajajay325@gmail.com>
2024-10-30 15:56:34 +0530

    Delete 202101619010094-Cec2-main/202101619010094-Cec2-main/Mobile directory

--- 202101619010094-Cec2-main/202101619010094-Cec2-main/Mobile/src/main/java/com/example/demo/Repository/MobileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Model.MobileModel;
-
-public interface MobileRepository extends JpaRepository<MobileModel,Integer>{
-
-}
